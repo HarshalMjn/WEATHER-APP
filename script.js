11d67d4e4af7b3f0ffc6505b1b2eecb33cc39a99
HarhslaMjn <mahajanh006@gmail.com>
2023-07-31 19:38:00 +0530

    API Call

--- script.js
+++ script.js
@@ -1,29 +1,81 @@
-console.log('Hello Jee babbar');
 
-const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
+const API_KEY = "";
 
 function renderWeatherInfo(data) {
-        let newPara = document.createElement('p');
-        newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`
+    let newPara = document.createElement('p');
+    newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`
     
-        document.body.appendChild(newPara);
-}
+    document.body.appendChild(newPara);
+} 
 
 async function fetchWeatherDetails() {
-
     try {
-        let city = "goa";
+        let city = 'goa';
 
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
         const data = await response.json();
-    
-        console.log("Weather data:-> " , data);
+
+        console.log("weather data:->",data);
 
         renderWeatherInfo(data);
     }
     catch(err) {
         //handle the error here
+
     }
-    //https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric
+}
+
+async function getCustomWeatherDetails() {
+    try {
+        let latitude = 17.6333;
+        let longitude = 18.3333;
+
+        let result =  await fetch (`https://api.openweathermap.org/data/2.5/weather?
+                                 lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+
+        let data = await result.json();
+
+        console.log(data);
+
+    }
+    catch(err) {
+        console.log("Error Found",err);
+    }
+}
+
+function switchTab(clickedTab) {
+
+    apiErrorContainer.classList.remove("active");
+
+    if(clickedTab != currentTab) {
+        currentTab.classList.remove("current-tab");
+        currentTab = clickedTab;
+        currentTab.classList.add("current-tab");
+
+        if (!searchForm.classList.contains("active")) {
+            userInfoContainer.classList.remove("active");
+            grantAccessContainer.classList.remove("active");
+            searchForm.classList.add("active");
+          } 
+          else {
+            searchForm.classList.remove("active");
+            userInfoContainer.classList.remove("active");
+            //getFromSessionStorage();
+          }
+    }
+}
+ function getLocation() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else {
+        console.log("No geoLocation Support");
+    }
+}function showPosition(postion) {
+    let lat = postion.coords.latitude;
+    let longi = postion.coords.longitude;
+
+    console.log(lat);
+    console.log(logi);
 
 }
